4288a1f51b4c95ef20789f521d98de46b0e6a37a
Marty Rose <mrose@accertify.com>
2014-02-06 15:28:20 -0600

    update

---
+++ src/main/java/com/accertify/prime/SemiPrimeFactor.java
@@ -0,0 +1,49 @@
+package com.accertify.prime;
+
+import com.accertify.math.BigInteger;
+import com.accertify.random.MersenneTwister;
+import com.accertify.util.Log;
+import com.accertify.util.LogFactory;
+import java.security.SecureRandom;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: mrose
+ * Date: 2/15/12
+ * Time: 9:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SemiPrimeFactor {
+    protected static transient Log log = LogFactory.getLog(SemiPrimeFactor.class);
+
+
+    public static void main(String[] args) {
+        byte[] seed = SecureRandom.getSeed(8);
+        MersenneTwister twister = new MersenneTwister(toLong(seed));
+        BigInteger prime1 = BigInteger.probablePrime(32, twister);
+        BigInteger prime2 = BigInteger.probablePrime(32, twister);
+        log.warn(prime1);
+        log.warn(prime2);
+        BigInteger semiprime = prime1.multiply(prime2);
+
+        log.warn(semiprime);
+    }
+
+
+    public static long toLong(byte[] data) {
+        if (data == null || data.length != 8) return 0x0;
+        // ----------
+        return (
+                // (Below) convert to longs before shift because digits
+                //         are lost with ints beyond the 32-bit limit
+                (long) (0xff & data[0]) << 56 |
+                        (long) (0xff & data[1]) << 48 |
+                        (long) (0xff & data[2]) << 40 |
+                        (long) (0xff & data[3]) << 32 |
+                        (long) (0xff & data[4]) << 24 |
+                        (long) (0xff & data[5]) << 16 |
+                        (long) (0xff & data[6]) << 8 |
+                        (long) (0xff & data[7]) << 0
+        );
+    }
+}
