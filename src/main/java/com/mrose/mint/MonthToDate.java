4fe2575d68030728431de70db7246a6ee1b8a531
Martin Rose <martinrose@google.com>
2015-11-10 14:39:56 -0600

    more commit

--- src/main/java/com/mrose/mint/MonthToDate.java
+++ src/main/java/com/mrose/mint/MonthToDate.java
@@ -9,6 +9,7 @@ import au.com.bytecode.opencsv.CSVReader;
 import com.mrose.financial.LoadFinancialData;
 
 import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
 import org.joda.time.DateTimeConstants;
 import org.joda.time.YearMonth;
 import org.joda.time.format.DateTimeFormat;
@@ -43,7 +44,9 @@ public class MonthToDate {
   public static void main(String[] args) throws Exception {
     CSVReader reader = new CSVReader(new FileReader(FILE_PATH));
     Iterable<String[]> allRows = reader.readAll();
+    double percentInMonth = ((double) DateTime.now().getDayOfMonth()) / 30.0d;
 
+    System.out.println("Percent of Month Complete: " + percentInMonth);
     // Skip the header/prefix row
     allRows = Iterables.filter(allRows, new Predicate<String[]>() {
       @Override
@@ -101,20 +104,69 @@ public class MonthToDate {
           continue;
         } else {
           log.warn("Unable to categorize: " + mr.toString());
-          category="OTHER";
+          category = "OTHER";
         }
       }
 
-      categorize.putIfAbsent(category, new ArrayList<MintRow>());
+      if (!categorize.containsKey(category)) {
+        categorize.put(category, new ArrayList<>());
+      }
       categorize.get(category).add(mr);
     }
+    // Remove things that are just shuffling cash around between my accounts
     categorize.remove("NET");
+    categorize.remove("PAYCC");
+
+    // Remove Income
     categorize.remove("MRINCOME");
     categorize.remove("SRINCOME");
-    categorize.remove("PAYCC");
-    for(String key: categorize.keySet()) {
-      log.warn("category=" + key + " amount=" + sum(categorize.get(key)));
+
+    // Categories that don't make sense on a month to month basis
+    categorize.remove("TRAVEL");
+    categorize.remove("MORTGAGE");
+
+    StringBuilder onTrack = new StringBuilder();
+    StringBuilder offTrack = new StringBuilder();
+    StringBuilder overTrack = new StringBuilder();
+
+    for (String category : categorize.keySet()) {
+      if (Category.valueOf(category) == null) {
+        throw new IllegalArgumentException("Unknown category type: " + category);
+      }
+
+
+      Category ccc = Category.valueOf(category);
+
+      // This will be negative
+      BigDecimal monthToDate = sum(categorize.get(category));
+      // This will be positive
+      long budgetExpected = (long) (((double) ccc.getAmount()) * percentInMonth);
+
+      // Negative if overspent, positive if money left
+      long remainingMoney = ccc.getAmount() + monthToDate.longValue();
+      if (remainingMoney < 0) {
+        overTrack.append("OVEROVER: In " + category + " over by " + remainingMoney + " Spent " + monthToDate + " budget " + ccc
+                .getAmount() + "\n");
+      } else {
+        long remainingAgainstBudget = budgetExpected + monthToDate.longValue();
+        if (remainingAgainstBudget < 0) {
+          offTrack.append(
+              "NOT ON TRACK: In " + category + " there is " + remainingMoney + " left. Spent "
+                  + monthToDate + " budget " + ccc.getAmount() + "\n");
+          Collection<MintRow> entries = categorize.get(category);
+          for (MintRow mr : entries) {
+            offTrack.append(
+                "\t" + mr.getFinancialAmount() + " : " + dtf.print(mr.getDate()) + " : " + mr
+                    .getDescription() + "\n");
+          }
+        } else {
+          onTrack.append("ONTRACK: In " + category + " there is " + remainingMoney + " left.\n");
+        }
+      }
     }
+    System.out.println(onTrack.toString());
+    System.out.println(offTrack.toString());
+    System.out.println(overTrack.toString());
   }
 
   private static String cleanup(String s) {
@@ -124,7 +176,7 @@ public class MonthToDate {
   private static BigDecimal sum(Iterable<MintRow> mintRows) {
     BigDecimal sum = BigDecimal.ZERO;
 
-    for(MintRow mr: mintRows) {
+    for (MintRow mr : mintRows) {
       sum = sum.add(mr.getFinancialAmount());
     }
     return sum;
