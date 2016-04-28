cbbc47b9c1ffd279f8d791ab916ae1fab29b4ff8
Martin Rose <martinrose@google.com>
2016-04-28 15:44:00 -0500

    updates

--- src/main/java/com/mrose/mint/MonthToDate.java
+++ src/main/java/com/mrose/mint/MonthToDate.java
@@ -47,12 +47,12 @@ public class MonthToDate {
   // readlink -f file
   private static final String FILE_PATH = "/tmp/transactions.csv";
   private static final FinancialPeriod PRIMARY_PERIOD =
-      new FinancialPeriod(new YearMonth(2016, DateTimeConstants.MARCH).toInterval(), 1);
+      new FinancialPeriod(new YearMonth(2016, DateTimeConstants.APRIL).toInterval(), 1);
   private static final FinancialPeriod EXTENDED_PERIOD =
       new FinancialPeriod(
           new Interval(
               new YearMonth(2015, DateTimeConstants.NOVEMBER).toInterval().getStart(),
-              new YearMonth(2016, DateTimeConstants.FEBRUARY).toInterval().getEnd()),
+              new YearMonth(2016, DateTimeConstants.MARCH).toInterval().getEnd()),
           4);
 
   // 1/04/2012
