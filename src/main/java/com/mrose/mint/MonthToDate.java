af29469917b18c20f5fe70992da5b35409ca2f59
Marty Rose <martinrose@martinrose2.chi.corp.google.com>
2018-02-28 09:15:12 -0600

    Feb 2018

--- src/main/java/com/mrose/mint/MonthToDate.java
+++ src/main/java/com/mrose/mint/MonthToDate.java
@@ -47,14 +47,14 @@ public class MonthToDate {
   // readlink -f file
   private static final String FILE_PATH = "/tmp/transactions.csv";
   private static final FinancialPeriod PRIMARY_PERIOD =
-      new FinancialPeriod(new YearMonth(2018, DateTimeConstants.JANUARY).toInterval(), 1);
+      new FinancialPeriod(new YearMonth(2018, DateTimeConstants.FEBRUARY).toInterval(), 1);
   private static final FinancialPeriod EXTENDED_PERIOD =
       new FinancialPeriod(
           new Interval(
-              new YearMonth(2017, DateTimeConstants.JANUARY).toInterval().getStart(),
-              new YearMonth(2017, DateTimeConstants.DECEMBER).toInterval().getEnd()),
-          12);
-  private static boolean INCLUDE_LINE_DETAILS = true;
+              new YearMonth(2018, DateTimeConstants.JANUARY).toInterval().getStart(),
+              new YearMonth(2018, DateTimeConstants.FEBRUARY).toInterval().getEnd()),
+          2);
+  private static boolean INCLUDE_LINE_DETAILS = false;
 
   // 1/04/2012
   // http://joda-time.sourceforge.net/apidocs/org/joda/time/format/DateTimeFormat.html
